699ccfce6a4d3dc8cda76a0f20aacb7c4a78e60f
Ant-Quanique Dancy <antdancy95@gmail.com>
2019-12-15 21:49:07 -0500

    updated ui

---
+++ public/precache-manifest.f37f1780f589d0c4a99bce4d17751dac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c61dbecf39fbd1df6c3674e554fe0c8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cda4efddb01af803115",
+    "url": "/static/css/main.a76ebe43.chunk.css"
+  },
+  {
+    "revision": "152a65f6984b06ff6cd7",
+    "url": "/static/js/2.65d0fb23.chunk.js"
+  },
+  {
+    "revision": "0cda4efddb01af803115",
+    "url": "/static/js/main.f24edce6.chunk.js"
+  },
+  {
+    "revision": "252070e227e76f6a9d40",
+    "url": "/static/js/runtime-main.cb75c723.js"
+  }
+]);
