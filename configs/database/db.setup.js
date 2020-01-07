2d015c020add37a6901b32ca1622444dc4d3ca9b
Ant-Quanique Dancy <antdancy95@gmail.com>
2020-01-07 12:32:24 -0500

    moved the db config

--- configs/database/db.setup.js
+++ configs/database/db.setup.js
@@ -1,6 +1,6 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-mongoose
-  .connect(process.env.MONGODB_URI, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true})
-  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
-  .catch(err => console.error('Error connecting to mongo', err));
+// mongoose
+//   .connect(process.env.MONGODB_URI, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true})
+//   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+//   .catch(err => console.error('Error connecting to mongo', err));
