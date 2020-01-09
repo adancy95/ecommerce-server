d78bba6429b83f0761998b021236ceca350a91a7
Ant-Quanique Dancy <antdancy95@gmail.com>
2020-01-08 23:02:36 -0500

    added an order model

---
+++ models/Order.js
@@ -0,0 +1,36 @@
+const mongoose = require(mongoose)
+
+
+const Schema = new mongoose.Schema;
+
+const orderSchema = Schema({
+  firstName: String,
+  lastName: String,
+  emailAddress: String,
+  phone: String,
+  mailingAddressOne: String,
+  mailingAddressTwo: String,
+  city: String,
+  state: String,
+  country: String,
+  zipCode: Number,
+  sizeType: {
+    type: String,
+    enum: ["Kids", "Youth", "Women", "Men"]
+  },
+  sizes: String,
+  colors: {
+    type: String,
+    enum: ["White", "Black", "Gray", "Red", "Blue", "Other"]
+  },
+  shirtFit: {
+    type: String,
+    enum: ["Fitted", "Loose Fit", "Round Neck", "V-Neck"]
+  },
+  designImage: String,
+  designText: String,
+  quantity: Number
+})
+
+const Order = mongoose.model("Order", orderSchema);
+module.exports = Order
