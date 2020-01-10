5a5a98fcdc647cc6ab066f34576f0594d40e182d
Ant-Quanique Dancy <antdancy95@gmail.com>
2020-01-10 08:12:30 -0500

    added create order route

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -4,6 +4,20 @@ const Order = require('../models/Order')
 const uploadMiddleWare = require('../configs/images/cloudinary');
 
 
+orderRouter.post("/api/orders/create", uploadMiddleWare.single('designImage'), (req, res, next) => {
+  let newOrder = req.body;
+  newOrder.designImage = 'No image was uploaded';
+  if (req.file) {
+    newOrder.designImage = req.file.url;
+  }
+
+  Order.create(newOrder)
+    .then(order => {
+      res.status(200).json({order})
+    })
+    .catch(err => next(err))
+})
+
 orderRouter.get("/api/orders", (req, res, next) => {
   Order.find()
     .then(orders => {
