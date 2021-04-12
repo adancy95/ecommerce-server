14844876a5999fcacd89f2d78c22e927b7c55d3d
adancy95 <antdancy95@gmail.com>
2021-04-12 18:48:06 -0400

    edited db connection string

--- configs/database/db.setup.js
+++ configs/database/db.setup.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(process.env.MONGODB_URI, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true})
-  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
-  .catch(err => console.error('Error connecting to mongo', err));
+  .connect(process.env.MONGODB_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true
+  })
+  .then(x => console.log(`Connected to Mongo!`))
+  .catch(err => console.error("Error connecting to mongo", err));
